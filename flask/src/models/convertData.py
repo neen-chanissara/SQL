aa86eff2c419ffb16c3f9b747de65c80fc45e439
neen-chanissara <chanissara.neen@gmail.com>
2020-02-24 21:57:21 +0700

    Delete File Flask / Node

--- flask/src/models/convertData.py
+++
@@ -1,8 +0,0 @@
-from decimal import Decimal
-import json
-
-def dictDecimalToInt(data_obj):
-    for k, v in data_obj.items():
-        if(type(v) == type(Decimal())):
-            data_obj[k] = int(v)
-    return data_obj
